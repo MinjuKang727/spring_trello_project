b769940663a58d6116039981676a28f9da068b58
Bongwon <movieid@naver.com>
2024-10-14 16:05:59 +0900

    init commit

---
+++ src/main/java/com/sparta/springtrello/SpringTrelloApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.springtrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrelloApplication.class, args);
+    }
+
+}
