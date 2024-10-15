da6ad15a2dcf0f1cf98db43f4948f000fa9127ee
zenoWS <zeno9302@naver.com>
2024-10-15 19:53:18 +0900

    Add : 비즈니스로직 삭제여부 업데이트 보드 생성자 추가

--- src/main/java/com/sparta/springtrello/domain/board/entity/Board.java
+++ src/main/java/com/sparta/springtrello/domain/board/entity/Board.java
@@ -1,9 +1,15 @@
 package com.sparta.springtrello.domain.board.entity;
 
+import com.sparta.springtrello.domain.workspace.entity.Workspace;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Board {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,11 +22,29 @@ public class Board {
     @JoinColumn(name = "workspace_id")
     private Workspace workspace;
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
-    private List<Deck> Deck = new ArrayList<>();
+//    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+//    private List<Deck> Deck = new ArrayList<>();
 
-    private String backgroundcolor;
-    private String backgroundimage;
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
 
+    private String backgroundColor;
+    private String backgroundImage;
+    
+    public Board(String title, Workspace workspace, String backgroundColor, String backgroundImage) {
+        this.title = title;
+        this.workspace = workspace;
+        this.backgroundColor = backgroundColor;
+        this.backgroundImage = backgroundImage;
+    }
 
+    public void updateBoard(String title, String backgroundColor, String backgroundImage) {
+        this.title = title;
+        this.backgroundColor = backgroundColor;
+        this.backgroundImage = backgroundImage;
+    }
+
+    public void Deleted() {
+        this.isDeleted = true;
+    }
 }
