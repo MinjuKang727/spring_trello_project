35f1bbf28ab7e08f25188fc8d4ec20623ec490a7
MinjuKang <minjukang727@gmail.com>
2024-10-17 20:20:07 +0900

    feat/notification : 특정 컨텐츠 레포지토리를 필요할 때, 주입받기 위해 ContentFactory 만듦
    슬랙 알람 서비스에서 이벤트별로 컨텐츠의 레포지토리를 조회하려면 한번에 모든 레포지토리를 주입받아야하는데
    이벤트마다 모든 레포지토리가 필요한 것은 아니어서
    필요한 레포지토리만 주입받고 싶어서 처음엔 repositoryFactory를 만듦.
    처음에는 모든 레포지토리를 생성자 주입으로 받고 있었는데
    그렇게 하면 서비스에서 레포지토리 주입받는 거랑 다를 게 없는 것 같아서
    다른 방법을 찾아 봄.
    @Lookup 어노테이션으로 필요한 때에 해당 Bean 객체를 가져올 수 있다는 것을 알게됨.(적용)
    contentId로 이벤트 발생 컨텐츠 객체를 반환 받고 싶어서 getContent() 메서드를 추가,
    StringBuffer 객체로 이벤트 발생 기본 문구와 이벤트 발생 컨텐츠 정보를 합쳐 알람 메세지를 만드는 getMessage() 메서드 추가

---
+++ src/main/java/com/sparta/springtrello/domain/notification/repository/ContentFactory.java
@@ -0,0 +1,163 @@
+package com.sparta.springtrello.domain.notification.repository;
+
+import com.sparta.springtrello.common.ErrorStatus;
+import com.sparta.springtrello.common.exception.ApiException;
+import com.sparta.springtrello.domain.board.entity.Board;
+import com.sparta.springtrello.domain.board.repository.BoardRepository;
+import com.sparta.springtrello.domain.card.entity.Card;
+import com.sparta.springtrello.domain.card.repository.CardRepository;
+import com.sparta.springtrello.domain.deck.entity.Deck;
+import com.sparta.springtrello.domain.deck.repository.DeckRepository;
+import com.sparta.springtrello.domain.member.entity.Member;
+import com.sparta.springtrello.domain.member.repository.MemberRepository;
+import com.sparta.springtrello.domain.notification.enums.NotificationCategory;
+import com.sparta.springtrello.domain.workspace.entity.Workspace;
+import com.sparta.springtrello.domain.workspace.repository.WorkspaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ContentFactory {
+
+    private StringBuffer sb = new StringBuffer();
+
+    @Lookup
+    private WorkspaceRepository getWorkspaceRepository() {
+        return null;
+    }
+
+    @Lookup
+    private BoardRepository getBoardRepository() {
+        return null;
+    }
+
+    @Lookup
+    private DeckRepository getDeckRepository() {
+        return null;
+    }
+
+    @Lookup
+    private CardRepository getCardRepository() {
+        return null;
+    }
+
+    @Lookup
+    private MemberRepository getMemberRepository() {
+        return null;
+    }
+
+    public Object getContentRepository(NotificationCategory category) {
+        return switch (category) {
+            case WORKSPACE -> getWorkspaceRepository();
+            case BOARD -> getBoardRepository();
+            case DECK -> getDeckRepository();
+            case CARD -> getCardRepository();
+            case MEMEBER -> getMemberRepository();
+            default -> throw new ApiException(ErrorStatus.BAD_REQUEST_CONTENT_CATEGORY);
+        };
+    }
+
+    public Object getContent(NotificationCategory category, Long id) {
+        return switch (category) {
+            case WORKSPACE -> getWorkspaceRepository()
+                    .findById(id)
+                    .orElseThrow(
+                            () -> new ApiException(ErrorStatus.NOT_FOUND_WORKSPACE)
+                    );
+            case BOARD -> getBoardRepository()
+                    .findById(id)
+                    .orElseThrow(
+                            () -> new ApiException(ErrorStatus.NOT_FOUND_BOARD)
+                    );
+            case DECK -> getDeckRepository()
+                    .findById(id)
+                    .orElseThrow(
+                            () -> new ApiException(ErrorStatus.NOT_FOUND_DECK)
+                    );
+            case CARD -> getCardRepository()
+                    .findById(id)
+                    .orElseThrow(
+                            () -> new ApiException(ErrorStatus.NOT_FOUND_CARD)
+                    );
+            case MEMEBER -> getMemberRepository()
+                    .findById(id)
+                    .orElseThrow(
+                            () -> new ApiException(ErrorStatus.NOT_FOUND_MEMBER)
+                    );
+            default -> throw new ApiException(ErrorStatus.BAD_REQUEST_CONTENT_CATEGORY);
+        };
+    }
+
+    public String getMessage(String message, NotificationCategory category, Object content) {
+        sb.append(message);
+
+        return switch (category) {
+            case WORKSPACE -> getWorkspaceInfo((Workspace) content);
+            case BOARD -> getBoardInfo((Board) content);
+            case DECK -> getDeckInfo((Deck) content);
+            case CARD -> getCardInfo((Card) content);
+            case MEMEBER -> getMemberInfo((Member) content);
+            default -> throw new ApiException(ErrorStatus.BAD_REQUEST_CONTENT_CATEGORY);
+        };
+    }
+
+    private String getMemberInfo(Member member) {
+        sb.append("\n워크스페이스 이름 : ");
+        sb.append(member.getWorkspace().getName());
+        sb.append("\n멤버 닉네임 : ");
+        sb.append(member.getUser().getNickname());
+        sb.append("  | 유저 권한 : ");
+        sb.append(member.getUser().getUserRole());
+        sb.append("  |  멤버 권한 : ");
+        sb.append(member.getMemberRole());
+
+        return sb.toString();
+    }
+
+    private String getCardInfo(Card card) {
+        sb.append("\n워크스페이스 이름 : ");
+        sb.append(card.getDeck().getBoard().getWorkspace().getName());
+        sb.append("  |  보드 이름 : ");
+        sb.append(card.getDeck().getBoard().getTitle());
+        sb.append("\n덱 이름 : ");
+        sb.append(card.getDeck().getName());
+        sb.append("  |  카드 이름 : ");
+        sb.append(card.getTitle());
+
+        return sb.toString();
+    }
+
+    private String getDeckInfo(Deck deck) {
+        sb.append("\n워크스페이스 이름 : ");
+        sb.append(deck.getBoard().getWorkspace().getName());
+        sb.append("  |  보드 이름 : ");
+        sb.append(deck.getBoard().getTitle());
+        sb.append("\n덱 이름 : ");
+        sb.append(deck.getName());
+
+        return sb.toString();
+    }
+
+    private String getBoardInfo(Board board) {
+        sb.append("\n워크스페이스 이름 : ");
+        sb.append(board.getWorkspace().getName());
+        sb.append("  |  보드 이름 : ");
+        sb.append(board.getTitle());
+
+        return sb.toString();
+    }
+
+    private String getWorkspaceInfo(Workspace workspace) {
+        sb.append("\n워크스페이스 이름 : ");
+        sb.append(workspace.getName());
+        sb.append("\n관리자 : ");
+        sb.append(workspace.getUser().getNickname());
+
+        return sb.toString();
+    }
+
+
+
+}
