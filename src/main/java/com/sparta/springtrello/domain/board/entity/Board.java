b6af2d53d28c8fa580298a20df679e825c51bc27
MinjuKang <minjukang727@gmail.com>
2024-10-17 18:39:03 +0900

    feat/board : 보드 엔티티 코드 추가
    - isDeleted 필드 추가 : 보드 삭제 여부
    - 생성자 추가
    - updateBoard() 메서드 추가 : 보드 수정 메서드
    - delete() 메서드 추가 : 보드 삭제 메서드

--- src/main/java/com/sparta/springtrello/domain/board/entity/Board.java
+++ src/main/java/com/sparta/springtrello/domain/board/entity/Board.java
@@ -4,13 +4,14 @@ import com.sparta.springtrello.domain.deck.entity.Deck;
 import com.sparta.springtrello.domain.workspace.entity.Workspace;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Board {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,14 +21,33 @@ public class Board {
     @Column(nullable = false)
     private String title;
 
+    private boolean isDeleted;
+
     @ManyToOne
     @JoinColumn(name = "workspace_id")
     private Workspace workspace;
   
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderColumn(name = "deckk_order")  // 순서 칼럼 지정
+    @OrderColumn(name = "deck_order")  // 순서 칼럼 지정
     private List<Deck> deckList = new ArrayList<>();
 
     private String backgroundcolor;
     private String backgroundimage;
+
+    public Board(String title, Workspace workspace, String backgroundcolor, String backgroundimage) {
+        this.title = title;
+        this.workspace = workspace;
+        this.backgroundcolor = backgroundcolor;
+        this.backgroundimage = backgroundimage;
+    }
+
+    public void updateBoard(String title, String backgroundcolor, String backgroundimage) {
+        this.title = title;
+        this.backgroundcolor = backgroundcolor;
+        this.backgroundimage = backgroundimage;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
 }
